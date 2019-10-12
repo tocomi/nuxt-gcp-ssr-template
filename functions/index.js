7c8be379a630a03c4763bb39e10dab97c7b959c4
tocomi <tocomi@wish.ocn.ne.jp>
2019-10-12 12:55:21 +0900

    make function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
 const functions = require('firebase-functions');
 const { Nuxt } = require('nuxt')
+const app = require('express')()
+const nuxt = new Nuxt({
+  dev: false,
+  buildDir: '.nuxt',
+  build: {
+    publicPath: '/'
+  },
+})
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.app = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+app.use((request, response) => {
+  nuxt.renderRoute('/', { request }).then((result) => {
+    console.log(result.html)
+    response.send(result.html)
+  }).catch((error) => {
+    response.send(error)
+  })
+})
+
+exports.app = functions.https.onRequest(app)
